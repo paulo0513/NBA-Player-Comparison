b8f96c0cb0690c2f25a2d47aaf93788edc5a71b2
Paul Oh <pauloh@Pauls-MBP.attlocal.net>
2021-04-21 09:24:23 -0500

    working thru second API/Appending

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ async function fetchPlayerID(firstname, lastname) {
   const url = `https://www.balldontlie.io/api/v1/players/?search=${firstname}%20${lastname}`
   try {
     const response = await axios.get(url)
-    console.log(response.data.id("lebron", "james"))
+    console.log(response.data.data[0].id)
+    fetchPlayerStats(response.data.data[0].id)
+    showPlayerStats(response.data.data[0].id)
     // return response
   }
   catch (error) {
@@ -21,28 +23,57 @@ async function fetchPlayerID(firstname, lastname) {
   }
 }
 
-fetchPlayerID()
+fetchPlayerID("kevin", "durant")
 
-// async function fetchPlayerStats(playerID) {
-//   const url = `https://www.balldontlie.io/api/v1/season_averages?player_ids[]=${playerID}`
+async function fetchPlayerStats(playerID) {
+  const url = `https://www.balldontlie.io/api/v1/season_averages?player_ids[]=${playerID}`
+  try {
+    const response = await axios.get(url)
+    console.log(response.data.data[0])
+    return response
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
+
+// async function fetchPersonID(firstName, lastName) {
+//   const url = `http://data.nba.net/data/10s/prod/v1/2020/players.json`
 //   try {
 //     const response = await axios.get(url)
-//     fetchPlayerID()
+//     console.log(response.data.league.standard.${firstName}.${lastName})
 //     return response
 //   }
 //   catch (error) {
-//     console.log(error)
+//     console.error(error)
 //   }
 // }
 
-// const playerForm = document.querySelector('#form')
+// fetchPersonID("russell", "westbrook")
+
+// const playerForm = document.querySelector('form')
+// console.log(playerForm)
 
 // playerForm.addEventListener('submit', (e) => {
 //   e.preventDefault()
-//   const playerSearch = document.querySelector('#p1-fname, p1-lname').value
-//   document.querySelector('#p1-fname, #p1-lname').value = ""
-//   fetchPlayerID(playerSearch)
+//   const playerSearch = document.querySelector('.p1-fname, .p1-lname').value
+//   document.querySelector('.p1-fname, .p1-lname').value = ""
+//   fetchPlayerStats(playerSearch)
 // })
+
+function showPlayerStats(data) {
+  let playerData = `
+  <h3>Player Stats:</h3>
+  <div class="p1-points">Points: ${data.data[0].pts}</div>
+  <div class="p1-rebounds">Rebounds: ${data.data[0].reb}</div>
+  <div class="p1-assists">Assists: ${data.data[0].ast}</div>
+  <div class="p1-steals">Steals: ${data.data[0].stl}</div>
+  <div class="p1-turnovers">Turnovers: ${data.data[0].turnover}</div>
+  `
+
+  document.querySelector('.player-data').insertAdjacentHTML('beforeend', playerData)
+  return playerData
+}
 
 // Set player ID as a variable, call API again to fetch player stats
 
